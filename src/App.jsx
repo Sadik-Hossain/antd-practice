a73810d745a8804454a7caa4b1780b09669a2b1a
sadik <po480373@gmai.com>
2022-06-27 10:06:31 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "antd/dist/antd.css";
+import { Button } from "antd";
+function App() {
+  return (
+    <div className="App">
+      <h1>hello </h1>
+      <Button type="primary">My btn</Button>
+    </div>
+  );
+}
+
+export default App;
